8a8f7e00b95fa75dacb735ef9248ac7242b8acbc
Laura Soledad Nocetti <93163751+launocetti@users.noreply.github.com>
2022-09-10 04:19:22 -0300

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,44 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+<br>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            
+
+            <li >
+                <a href="Vistapanel.php">
+                <i class="fa fa-server"></i> <span>Inicio</span>
+                <span class="pull-right-container">
+                </span>
+                </a>
+            </li>
+
+            <li >
+                <a href="Vistaproductos.php">
+                <i class="fa fa-gift"></i> <span>Productos</span>
+                <span class="pull-right-container">
+                </span>
+                </a>    
+            </li>
+            <li >
+                <a href="Vistaventas.php">
+                    <i class="fa fa-bar-chart-o"></i> <span>Ventas</span>
+                    <span class="pull-right-container">
+                    </span>
+                </a>
+            </li>
+            <li >
+                <a href="Vistausuarios.php">
+                    <i class="fa fa-user"></i> <span>Usuarios</span>
+                    <span class="pull-right-container">
+                    </span>
+                </a>
+            </li>
+        </ul>
+
+    </section>
+    <!-- /.sidebar -->
+    </aside>
+
